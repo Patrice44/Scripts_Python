9c6f424990611154b5edd0ba01a3eceb61e77ed3
Patrice44 <cosson.patrice@gmail.com>
2019-04-29 20:43:58 +0200

    Modifs instruction print / Compatibilite Python 3.x /suite_arithmetique_exercice3.1_devoir.py

--- suite_arithmetique_exercice3.1_devoir.py
+++ suite_arithmetique_exercice3.1_devoir.py
@@ -3,18 +3,18 @@
 #
 # Exercice 3.1
 # Suite arithmétique   Un+1 = Un + r
-print "Suite arithmétique   Un+1 = Un + r"
+print("Suite arithmétique   Un+1 = Un + r")
 U1=10
 r=-4
 n=5
 
 U0=U1-(1*r)
 U5=U0+(n*r)
-print 'U0  = ', U0
-print 'U1  = ', U1
-print 'r   = ', r
-print 'n   = ', n
-print 'U5 = U0+(n*r)= ', U5
+print('U0  = ', U0)
+print('U1  = ', U1)
+print('r   = ', r)
+print('n   = ', n)
+print('U5 = U0+(n*r)= ', U5)
 
 # S12=((n+1)*(U0+U12))/2
 # print 'S12 = ((n+1)*(U0+U12))/2 = ', S12
@@ -28,11 +28,11 @@ for i in range(0,n+1):
 	if i == 0:
 		Un=U0
 		ST=ST+Un
-		print i,'U',i, '= ', Un #, 'ST = ', ST
+		print(i,'U',i, '= ', Un) #, 'ST = ', ST
 	else:
 		Un=Un+r
 		ST=ST+Un
-		print i,'U',i, '= ', Un #, 'ST = ', ST
+		print(i,'U',i, '= ', Un) #, 'ST = ', ST
 exit()
 
 """
